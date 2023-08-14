2cd06f080b1372f97723e2d2667b131555514b50
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-14 20:40:28 +0300

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 
-	int i;
+int i;
 
-	for (i = 'a'; i <= 'f'; i++)
-	{
-		putchar(i);
-	}
-	for (i = 0; i < 10; i++)
-	{
-		putchar(i + 48);
-	}
-	putchar('\n');
-	return (0);
+for (i = 0; i < 10; i++)
+{
+	putchar(i + 48);
+}
+for (i = 'a'; i <= 'f'; i++)
+{
+	putchar(i);
+}
+putchar('\n');
+return (0);
 }
