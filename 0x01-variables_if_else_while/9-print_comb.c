7bb0df1d2a2112652e48f56b398ad10b968f0a06
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-14 21:09:40 +0300

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		putchar(i + 48);
+		putchar(i);
 		putchar(',');
+		putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 }
