a83065d0f057873b7907603c04e9f6a178255d8b
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-14 18:00:41 +0300

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - print the alphabets except q and e.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	i = 97;
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
