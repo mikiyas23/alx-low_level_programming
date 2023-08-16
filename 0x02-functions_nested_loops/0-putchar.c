ef32f70c0f77c3dee7634b2cd85be7c3c7b74722
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-16 11:33:12 +0300

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+char *c = "_putchar";
+while (*c != '\0')
+{
+	_putchar(*c);
+	c++;
+}
 _putchar('\n');
 return (0);
 }
