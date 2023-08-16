9189bb82ca15ea6c9567881ad079cc5cfeb1da27
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-16 12:32:34 +0300

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints out lower case with putchar from a to z
+*
+* Return:void
+*/
+
+void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 113;)
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+}
