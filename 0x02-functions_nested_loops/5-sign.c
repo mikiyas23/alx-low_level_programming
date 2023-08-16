09320cc5ffeccbcf1eab19653f3803061c3405df
mikiyas23 <mikiyasamare23@gmail.com>
2023-08-16 17:08:41 +0300

    5-sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_sign - describes check for alphabet character
- *@c: the character to be checked
+ * @n: the character to be checked
  * Return: 1 for alphabetic character or 0 for anything
  */
+
 int print_sign(int n)
 {
 if (n > 0)
